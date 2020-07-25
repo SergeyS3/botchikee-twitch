91b77a38eee609a1b846170191c095c931310e7e
Sergey Semichastnov <Sergey--S@yandex.ru>
2020-07-25 17:37:06 +0300

    logging

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const Bot = new Botchikee()
 
 ;(async () => {
 	try {
+		Bot.on('ws_in', msg => console.log(`> ${msg}`) )
+		
 		await Bot.connect()
+		
+		Bot.on('ws_out', msg => console.log(`< ${msg}`) )
+		
 		Bot.join('airchikee')
 	} catch (e) {
 		console.error(e)
