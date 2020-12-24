445e236e6d102707aacfa215873221f4f669dd9a
Сергей Семичастнов <Sergey--S@yandex.ru>
2020-12-25 02:29:37 +0300

    answers from database

--- modules/Answer.js
+++ modules/Answer.js
@@ -1,15 +1,32 @@
-const ChatMessageParser = require('../tools/ChatMessageParser')
+const ChatMessage = require('../tools/ChatMessage')
+const mongoose = require('mongoose')
+const AnswerModel = require('../models/answer')
 
 class Answer {
-	constructor(Client) {
-		Client.on('msg_in', (channel, user, msg) => {
-			if(user == this.username)
-				return
-			
-			const answer = new ChatMessageParser(channel, user, msg).findAnswer()
-			if(answer)
-				Client.say(channel, answer)
+	constructor(Client, params) {
+		this.Client = Client
+		this.params = params
+		Client.on('msg_in', this.msgIn.bind(this))
+	}
+	async setAnswers() {
+		await mongoose.connect('mongodb://localhost/botchikee', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
 		})
+		
+		this.answers = await AnswerModel.find()
+	}
+	async msgIn(channel, user, msg) {
+		if(user == this.username)
+			return
+		
+		if(!this.answers)
+			await this.setAnswers()
+		
+		const chatMessage = new ChatMessage(channel, user, msg)
+		const answer = await chatMessage.makeAnswer(this.answers)
+		if(answer)
+			this.Client.say(channel, answer)
 	}
 }
 
