7347c0f72c9ceb04637807c33d813218684fae87
Sergey Semichastnov <Sergey--S@yandex.ru>
2021-10-28 17:34:18 +0300

    auto reconnect

--- clients/Client.js
+++ clients/Client.js
@@ -5,6 +5,14 @@ const debug = require('debug')('ws')
 const tokens = require('../data/tokens')
 
 class Client extends EventEmitter {
+	wasCloseCalled = false
+	reconnectTimer = {
+		current: 1000,
+		min: 1000,
+		max: 3600000
+	}
+	channels = []
+
 	constructor(username) {
 		super()
 		this.username = username
@@ -16,9 +24,9 @@ class Client extends EventEmitter {
 	connect() {
 		this.ws = new ws('ws://irc-ws.chat.twitch.tv:80/', 'irc')
 		
-		this.ws.onmessage = e => e.data.split("\r\n").forEach(m => m && this.processMessage(m))
-		this.ws.onerror = e => console.error(e)
-		this.ws.onclose = e => console.error(e)
+		this.ws.onmessage = e => e.data.split("\r\n").forEach(m => m && this.onMessage(m))
+		this.ws.onerror = this.onError.bind(this)
+		this.ws.onclose = this.onClose.bind(this)
 		
 		return new Promise(resolve => {
 			this.ws.onopen = () => {
@@ -30,6 +38,34 @@ class Client extends EventEmitter {
 		})
 	}
 	
+	disconnect() {
+		this.unsetPingLoop()
+		
+		return new Promise(resolve => {
+			if(this.ws.readyState <= this.ws.OPEN) {
+				this.wasCloseCalled = true
+				this.once('disconnected', resolve)
+				
+				this.ws.close(1000)
+			}
+			else
+				resolve()
+		})
+	}
+	
+	reconnect() {
+		this.reconnectTimer.current = Math.min(this.reconnectTimer.current * 2, this.reconnectTimer.max)
+		
+		setTimeout(async () => {
+			await this.disconnect()
+			await this.connect()
+			
+			this.channels.forEach(c => this.join(c))
+			
+			this.reconnectTimer.current = this.reconnectTimer.min
+		}, this.reconnectTimer.current)
+	}
+	
 	send(str) {
 		debug(`< ${str}`)
 		
@@ -39,10 +75,15 @@ class Client extends EventEmitter {
 	
 	join(channel) {
 		this.send(`JOIN #${channel}`)
+		
+		if(!this.channels.includes(channel))
+			this.channels.push(channel)
 	}
 	
 	part(channel) {
 		this.send(`PART #${channel}`)
+		
+		this.channels = this.channels.filter(c => c !== channel)
 	}
 	
 	say(channel, msg) {
@@ -50,8 +91,8 @@ class Client extends EventEmitter {
 		this.send(`PRIVMSG #${channel} :${msg}`)
 	}
 	
-	processMessage(msg) {
-		debug(`< ${msg}`)
+	onMessage(msg) {
+		debug(`> ${msg}`)
 
 		const parsedMsg = IrcParser.parse(msg)
 		const {command, channel, user, params, tags} = parsedMsg
@@ -64,9 +105,13 @@ class Client extends EventEmitter {
 				break
 			case '372':
 				this.emit('connected')
+				this.setPingLoop()
 				break
 			case 'PING':
 				this.send('PONG :tmi.twitch.tv')
+				return
+			case 'PONG':
+				this.resetPingLoop()
 				return
 			case 'JOIN':
 				if(user === this.username)
@@ -79,7 +124,45 @@ class Client extends EventEmitter {
 			case 'PRIVMSG':
 				this.emit('msg_in', channel, user, params[1].trim(), tags)
 				break
+			case 'RECONNECT':
+				this.reconnect()
+				break
 		}
+	}
+	
+	onError(e) {
+		console.error('ws error. Code: ' + e.code)
+	}
+	
+	onClose(e) {
+		this.emit('disconnected')
+		
+		if(this.wasCloseCalled)
+	        this.wasCloseCalled = false
+	    else {
+	    	console.error('ws unexpected close. Code: ' + e.code)
+	    	this.reconnect()
+		}
+	}
+	
+	setPingLoop() {
+		this.pingLoop = setInterval(() => {
+            this.send('PING')
+            this.pingTimeout = setTimeout(() => {
+                this.unsetPingLoop()
+	            console.error('ping timeout')
+                this.reconnect()
+            }, 10000)
+        }, 60000)
+	}
+	
+	resetPingLoop() {
+		clearTimeout(this.pingTimeout)
+	}
+	
+	unsetPingLoop() {
+		clearInterval(this.pingLoop)
+		this.resetPingLoop()
 	}
 }
 
