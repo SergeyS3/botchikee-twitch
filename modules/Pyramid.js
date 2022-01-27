ee15d94ac97c80cbade610f02759276898b8c209
Sergey Semichastnov <Sergey--S@yandex.ru>
2022-01-27 19:21:50 +0300

    pyramid with emoji

--- modules/Pyramid.js
+++ modules/Pyramid.js
@@ -1,5 +1,6 @@
 const Module = require('./Module')
 const MsgQueue = require('../submodules/MsgQueue')
+const emojiRegex = require('emoji-regex')()
 
 class Pyramid extends Module {
 	name = 'Pyramid'
@@ -31,27 +32,52 @@ class Pyramid extends Module {
 	}
 	
 	async msgIn(channel, user, msg) {
-		if(user.self || !this.checkChannel(channel) || !/^\w{3,15}$/.test(msg))
+		const queue = this.queue[channel].slice() //saving queue state on function call
+		
+		if(user.self || !this.checkChannel(channel) || !this.constructor.canBeEmote(msg))
 			return
 		
-		const pyramidWidth = this.getPyramidWidth(this.queue[channel], user.name, msg)
+		const pyramidWidth = this.constructor.getPyramidWidth(queue, user.name, msg)
 		if(pyramidWidth >= this.minWidth)
 			this.Client.say(channel, `nice ${pyramidWidth}-width ${msg} pyramid ${user.displayName} LUL`)
 	}
 	
-	getPyramidWidth(queue, buildername, word) {
+	static canBeEmote(msg) {
+		if([
+			'D:', 'R)', ';p', ':p',
+			';)', ':\\', '<3', ':O',
+			'B-)', 'B)', 'O_o', ':|',
+			'>(', ':D', ':-(', ':(',
+			':-)', ':)',
+		].includes(msg))
+			return true
+		
+		if(msg.match(/^\w{3,15}$/) && msg.match(/\D/))
+			return true
+		
+		const emojisMatch = msg.match(emojiRegex)
+		if(!emojisMatch)
+			return false
+		
+		if(emojisMatch.length === 1)
+			return true
+		
+		const emojiSequencesMatch = emojisMatch.join("\u200d").match(emojiRegex)
+		return emojiSequencesMatch?.length === 1
+	}
+	
+	static getPyramidWidth(queue, username, str) {
 		let checkWidth = 1
 		let pyramidWidth
 		let directionUp = true
 		
-		queue = queue.slice()
 		queue.pop()
 		
 		for(const { user, msg } of queue.reverse()) {
-			if(user.name !== buildername)
+			if(user.name !== username)
 				break
 			
-			const checkRow = () => msg === word + ` ${word}`.repeat(checkWidth - 1)
+			const checkRow = () => msg === str + ` ${str}`.repeat(checkWidth - 1)
 			
 			if(directionUp) {
 				checkWidth++
